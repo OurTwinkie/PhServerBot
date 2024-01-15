59e15e03dbb89016e45e69494e2f272f1b1a0ea7
OurTwinkie <qas1noikon@gmail.com>
2024-01-15 16:16:35 +0100

    Add Emoji.java

---
+++ src/main/java/org/twinkie/phbot/config/Emoji.java
@@ -0,0 +1,7 @@
+package org.twinkie.phbot.config;
+
+public class Emoji {
+    public static final String SUCCESS = "✅";
+    public static final String WARNING = "⚠️";
+    public static final String ERROR = "⛔️";
+}
