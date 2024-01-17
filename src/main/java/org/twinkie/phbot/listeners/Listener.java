cb783717e987534326459d2ed826fbd66a54062a
OurTwinkie <qas1noikon@gmail.com>
2024-01-17 20:28:34 +0100

    Add button ID check in Listener's onButtonInteraction method

--- src/main/java/org/twinkie/phbot/listeners/Listener.java
+++ src/main/java/org/twinkie/phbot/listeners/Listener.java
@@ -2,13 +2,20 @@ package org.twinkie.phbot.listeners;
 
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.jetbrains.annotations.NotNull;
+import org.twinkie.phbot.config.Constants;
 import org.twinkie.phbot.listeners.runnables.ButtonRunnable;
+
+import java.util.Objects;
 
 public class Listener extends ListenerAdapter {
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
-        ButtonRunnable.ruleAcceptButton(event, event.deferReply(true).complete());
+        Button button = event.getButton();
+        if (Objects.equals(button.getId(), Constants.ruleButtonId)) {
+            ButtonRunnable.ruleAcceptButton(event, event.deferReply(true).complete());
+        }
         super.onButtonInteraction(event);
     }
 
